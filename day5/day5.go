955bca2937768f10290faed4930b30dae4610490
Allie <allisonlarson91@gmail.com>
2020-12-05 13:07:59 -0800

    Day5 Neato Binary conversion

--- day5/day5.go
+++ day5/day5.go
@@ -1,44 +1,27 @@
 package day5
 
 import (
-	"bufio"
+	"aoc/util"
 	"io"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func Compute(r io.Reader, runPartTwo bool) (int, error) {
 	seatIds := []int{}
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		rows := make([]int, 128)
-		seat := make([]int, 8)
-		for i := range rows {
-			rows[i] = i
-		}
-		for i := range seat {
-			seat[i] = i
-		}
-
-		pass := scanner.Text()
-		for _, p := range pass {
-			seatChange := len(seat) / 2
-			rowChange := len(rows) / 2
-			if p == 'F' { // Lower half
-				rows = rows[:rowChange]
-			}
-			if p == 'B' { // Higher half
-				rows = rows[rowChange:]
-			}
-			if p == 'L' { // Lower half
-				seat = seat[:seatChange]
-			}
-			if p == 'R' { // Higher half
-				seat = seat[seatChange:]
-			}
+	input, err := util.ReadInput(r)
+	if err != nil {
+		return 0, err
+	}
+	for _, s := range strings.Fields(input) {
+		bin := strings.NewReplacer("F", "0", "B", "1", "L", "0", "R", "1").Replace(s)
+		seatId, err := strconv.ParseUint(bin, 2, 10)
+		if err != nil {
+			return 0, err
 		}
 
-		seatId := rows[0]*8 + seat[0]
-		seatIds = append(seatIds, seatId)
+		seatIds = append(seatIds, int(seatId))
 	}
 
 	sort.Ints(seatIds)
