155b74f5057160bf226950edc44a5f18371e6500
Allie <allisonlarson91@gmail.com>
2020-12-07 22:35:44 -0800

    Day2 pt2

--- day8/day8.go
+++ day8/day8.go
@@ -2,6 +2,7 @@ package day8
 
 import (
 	"bufio"
+	"errors"
 	"io"
 	"strconv"
 	"strings"
@@ -15,6 +16,7 @@ type Instruction struct {
 
 func Compute(r io.Reader, runPartTwo bool) (int, error) {
 	ins := []*Instruction{}
+	trades := []*Instruction{}
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		parts := strings.Fields(scanner.Text())
@@ -22,30 +24,66 @@ func Compute(r io.Reader, runPartTwo bool) (int, error) {
 		if err != nil {
 			return 0, nil
 		}
-		ins = append(ins, &Instruction{cmd: parts[0], mod: mod})
+		in := &Instruction{cmd: parts[0], mod: mod}
+		ins = append(ins, in)
+		if in.cmd == "nop" || in.cmd == "jmp" {
+			trades = append(trades, in)
+		}
 	}
+	acc, err := runCmds(ins)
+	if !runPartTwo {
+		return acc, nil
+	}
+
+	// Brute force the answer
+	for _, trade := range trades {
+		for _, ins := range ins {
+			ins.run = false
+		}
+
+		switch trade.cmd {
+		case "nop":
+			trade.cmd = "jmp"
+		case "jmp":
+			trade.cmd = "nop"
+		}
+
+		acc, err = runCmds(ins)
+		if err == nil {
+			return acc, err
+		}
+
+		switch trade.cmd {
+		case "nop":
+			trade.cmd = "jmp"
+		case "jmp":
+			trade.cmd = "nop"
+		}
+	}
+
+	return 0, errors.New("Couldn't find a solution")
+}
+
+func runCmds(ins []*Instruction) (int, error) {
 	acc := 0
 	i := 0
-	//max := len(ins)
 	currentIn := ins[i]
-	var oldIn *Instruction
 	for !currentIn.run {
-		oldIn = currentIn
 		switch currentIn.cmd {
 		case "acc":
 			acc += currentIn.mod
 			i++
-			currentIn = ins[i]
-			oldIn.run = true
 		case "nop":
 			i++
-			currentIn = ins[i]
-			oldIn.run = true
 		case "jmp":
 			i = i + currentIn.mod
-			currentIn = ins[i]
-			oldIn.run = true
 		}
+
+		if i >= len(ins) {
+			return acc, nil
+		}
+		currentIn.run = true
+		currentIn = ins[i]
 	}
-	return acc, nil
+	return acc, errors.New("Exited due to infinite loop")
 }
